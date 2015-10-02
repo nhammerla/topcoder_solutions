0be45c2f8e99efdffc5d758cbfa727dadd67fc05
Nils Hammerla <n.hammerla@gmail.com>
2015-10-02 10:44:39 +0100

    changing tabs to 2xwhitespace

--- GoodString.java
+++ GoodString.java
@@ -1,37 +1,37 @@
 // http://community.topcoder.com/stat?c=problem_statement&pm=13751
 public class GoodString {
-	public static String isGood(String s) {
-		// trivial case
-		if (s.length() % 2 != 0) {
-			// must be even length
-			return new String("Bad");
-		}
-		
-		if (s.length() == 0 || (s.length() > 0 && s.substring(0,1).equals("b"))) {
-			return new String("Bad");
-		}
-		
-		int as = 0;
-		int bs = 0;
-		for (int i = 0; i < s.length(); i++) {
-			if (s.substring(i,i+1).equals("a")) { as++; } 
-			else if (s.substring(i,i+1).equals("b")) { bs++; }
-		}
-		
-		if (as == bs) {
-			return "Good";
-		} else {
-			return "Bad";
-		}
-	}
-	
-	public static void main(String[] args) {
-		String s = new String("abaababababbaabbaaaabaababaabbabaaabbbbbbbb");
-		System.out.println(GoodString.isGood(s));
-		s = new String("aaaaaaaabbbaaabaaabbabababababaabbbbaabbabbbbbbabb");
-		System.out.println(GoodString.isGood(s));
-		s = new String("ababab");
-		System.out.println(GoodString.isGood(s));
-	}
-	
+  public static String isGood(String s) {
+    // trivial case
+    if (s.length() % 2 != 0) {
+      // must be even length
+      return new String("Bad");
+    }
+    
+    if (s.length() == 0 || (s.length() > 0 && s.substring(0,1).equals("b"))) {
+      return new String("Bad");
+    }
+    
+    int as = 0;
+    int bs = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if (s.substring(i,i+1).equals("a")) { as++; } 
+      else if (s.substring(i,i+1).equals("b")) { bs++; }
+    }
+    
+    if (as == bs) {
+      return "Good";
+    } else {
+      return "Bad";
+    }
+  }
+  
+  public static void main(String[] args) {
+    String s = new String("abaababababbaabbaaaabaababaabbabaaabbbbbbbb");
+    System.out.println(GoodString.isGood(s));
+    s = new String("aaaaaaaabbbaaabaaabbabababababaabbbbaabbabbbbbbabb");
+    System.out.println(GoodString.isGood(s));
+    s = new String("ababab");
+    System.out.println(GoodString.isGood(s));
+  }
+  
 }
--- ZigZag.java
+++ ZigZag.java
@@ -1,83 +1,83 @@
 // http://community.topcoder.com/stat?c=problem_statement&pm=1259&rd=4493
 class ZigZag {
-	public static int longestZigZag2(int[] sequence) {
-		// catch trivial case
-		if (sequence.length <= 1) {
-			// length of 0, or 1
-			return sequence.length;
-		} 
-		
-		int longest = 0;
-		int current = 0;
-		int lastChange = 0;
-		for (int i = 1; i < sequence.length; i++) {
-			if (sequence[i-1] < sequence[i]) {
-				if (lastChange == -1) {
-					current++;
-				} else {
-					if (current > longest) {
-						longest = current;
-					}
-					current = 1;
-				}
-				lastChange = 1;
-			} else if (sequence[i-1] > sequence[i]) {
-				if (lastChange == 1) {
-					current++;
-				} else {
-					if (current > longest) {
-						longest = current;
-					}
-					current = 1;
-				}
-				lastChange = -1;
-			} else { 
-				// equal
-				if (current > longest) {
-					longest = current;
-				}
-				current = 0;
-				lastChange = 0;
-			}
-			System.out.println(lastChange);
-		}
-		return longest;
-	} 
-	
-	public static int longestZigZag(int[] sequence) {
-		// catch trivial case
-		if (sequence.length <= 1) {
-			// length of 0, or 1
-			return sequence.length;
-		} 
-		
-		int longest = 0;
-		int lastChange = -2;
-		for (int i = 1; i < sequence.length; i++) {
-			if (sequence[i-1] > sequence[i]) {
-				if (lastChange == 1 || lastChange == -2) {
-					longest = (longest == 0) ? 2 : longest + 1;
-				} 
-				lastChange = -1;
-			} else if (sequence[i-1] < sequence[i]) {
-				if (lastChange == -1 || lastChange == -2) {
-					longest = (longest == 0) ? 2 : longest + 1; 
-				} 
-				lastChange = 1;
-			}
-		}
-		
-		return longest;
-	}
-	
-	public static void main(String[] args) {
-		int[] fooList = { 
-			374, 374, 40, 854, 203, 203, 156, 362, 279, 812, 955, 
-			600, 947, 978, 46, 100, 953, 670, 862, 568, 188, 
-			67, 669, 810, 704, 52, 861, 49, 640, 370, 908, 
-			477, 245, 413, 109, 659, 401, 483, 308, 609, 120, 
-			249, 22, 176, 279, 23, 22, 617, 462, 459, 244 
-			};
-		System.out.println(ZigZag.longestZigZag(fooList));
-	}
+  public static int longestZigZag2(int[] sequence) {
+    // catch trivial case
+    if (sequence.length <= 1) {
+      // length of 0, or 1
+      return sequence.length;
+    } 
+    
+    int longest = 0;
+    int current = 0;
+    int lastChange = 0;
+    for (int i = 1; i < sequence.length; i++) {
+      if (sequence[i-1] < sequence[i]) {
+        if (lastChange == -1) {
+          current++;
+        } else {
+          if (current > longest) {
+            longest = current;
+          }
+          current = 1;
+        }
+        lastChange = 1;
+      } else if (sequence[i-1] > sequence[i]) {
+        if (lastChange == 1) {
+          current++;
+        } else {
+          if (current > longest) {
+            longest = current;
+          }
+          current = 1;
+        }
+        lastChange = -1;
+      } else { 
+        // equal
+        if (current > longest) {
+          longest = current;
+        }
+        current = 0;
+        lastChange = 0;
+      }
+      System.out.println(lastChange);
+    }
+    return longest;
+  } 
+  
+  public static int longestZigZag(int[] sequence) {
+    // catch trivial case
+    if (sequence.length <= 1) {
+      // length of 0, or 1
+      return sequence.length;
+    } 
+    
+    int longest = 0;
+    int lastChange = -2;
+    for (int i = 1; i < sequence.length; i++) {
+      if (sequence[i-1] > sequence[i]) {
+        if (lastChange == 1 || lastChange == -2) {
+          longest = (longest == 0) ? 2 : longest + 1;
+        } 
+        lastChange = -1;
+      } else if (sequence[i-1] < sequence[i]) {
+        if (lastChange == -1 || lastChange == -2) {
+          longest = (longest == 0) ? 2 : longest + 1; 
+        } 
+        lastChange = 1;
+      }
+    }
+    
+    return longest;
+  }
+  
+  public static void main(String[] args) {
+    int[] fooList = { 
+      374, 374, 40, 854, 203, 203, 156, 362, 279, 812, 955, 
+      600, 947, 978, 46, 100, 953, 670, 862, 568, 188, 
+      67, 669, 810, 704, 52, 861, 49, 640, 370, 908, 
+      477, 245, 413, 109, 659, 401, 483, 308, 609, 120, 
+      249, 22, 176, 279, 23, 22, 617, 462, 459, 244 
+      };
+    System.out.println(ZigZag.longestZigZag(fooList));
+  }
 }
